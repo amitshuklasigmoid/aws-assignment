543b602df17913ca1a0b4c21f409cf2c0c299721
amitshuklasigmoid <122515454+amitshuklasigmoid@users.noreply.github.com>
2023-05-28 12:36:00 +0530

    Update task2.py

--- task2.py
+++ task2.py
@@ -6,19 +6,14 @@ from datetime import datetime
 def lambda_handler(event, context):
 
     BUCKET_NAME = 'amit1616'
-    now = str(datetime.now())
+    current_time = str(datetime.now())
 
-    data={}
 
-    data['transaction_id']=12345
-    data['payment_mode'] = 'card/netbanking/upi'
-    data['amount'] = 200
-    data['customer_id'] = 101
-    data['timestamp'] = now
+    data = {'transaction_id': 12345, 'payment_mode': "card/netbanking/upi", 'amount': 200.0,"customer_id":101,"timestamp":current_time}
 
     s3_client = boto3.client('s3')
 
-    file_name = now+'.json'
+    file_name = current_time+'.json'
     json_data=json.dumps(data)
 
     s3_client.put_object(Body=json_data, Bucket=BUCKET_NAME, Key=file_name)
